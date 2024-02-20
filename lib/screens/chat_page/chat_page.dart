248459595c63342d96bcc1d7c06433b02ddb0cc8
sangily <sangilyoon.dev@gmail.com>
2024-02-21 03:30:31 +0900

    채팅 서비스 제외 - TODO로 넘김

--- lib/screens/chat_page/chat_page.dart
+++ lib/screens/chat_page/chat_page.dart
@@ -16,29 +16,8 @@ class _ChatPageState extends State<ChatPage> {
         appBar: AppBar(
           title: Text("메신저"),
           backgroundColor: Colors.white,
-          bottom: TabBar(
-            tabs: const [Tab(text: "채팅"), Tab(text: "메세지")],
-            indicatorWeight: 3.0,
-            indicatorColor: AppColors.accentColor,
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicator: UnderlineTabIndicator(
-              borderSide: BorderSide(width: 2.0, color: AppColors.accentColor),
-              insets: EdgeInsets.symmetric(horizontal: 16.0),
-            ),
-            labelColor: Color(0xFF555555),
-            unselectedLabelColor: Color(0xDD666666).withOpacity(0.7),
-            labelStyle: const TextStyle(
-                fontWeight: FontWeight.bold),
-            unselectedLabelStyle: const TextStyle(
-                fontWeight: FontWeight.normal),
-          ),
         ),
-        body: const TabBarView(
-          children: [
-            Center(child: Text('채팅 페이지')),
-            Center(child: Text('메세지 페이지')),
-          ],
-        ),
+        body: const Center(child: Text('메세지 페이지')),
       ),
     );
   }
